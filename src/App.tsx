3689b5aa153ea342a3c3e201d097fb47519056f8
Greta Hachigian-Kreutzer <gretahk@bu.edu>
2025-09-29 15:54:28 -0400

    mp2 finished

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import DisneyCharacters from "./components/DisneyCharacters.tsx";
+import styled from "styled-components";
+import {useEffect, useState} from "react";
+import {Character} from "./interfaces/Charcters.ts";
+
+const ParentDiv=styled.div`
+    width: 80vw;
+    margin: auto;
+    border: 5px darkblue solid;
+`;
+
+export default function App(){
+    // useState hook stores data
+    const [data, setData] = useState<Character[]>([]);
+
+    // useEffect hook for error handling and re-rendering
+    useEffect(() => {
+        async function fetchData(): Promise<void> {
+            const rawData = await fetch("https://api.disneyapi.dev/character");
+            const {data} : {data: Character[]} = await rawData.json();
+            // filters for characters with images only
+            const charactersWithImages = data.filter(char => char.imageUrl);
+            setData(charactersWithImages);
+        }
+
+        fetchData()
+            .then(() => console.log("Data fetched successfully"))
+            .catch((e: Error) => console.log("There was the error: " + e));
+    }, [data.length]);
+
+
+        return(
+        <ParentDiv>
+            <DisneyCharacters data={data}/>
+        </ParentDiv>
+    )
+}
---
+++ src/components/DisneyCharacters.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import {Character} from "../interfaces/Charcters.ts";
+
+const AllCharsDiv=styled.div`
+    display: flex;
+    flex-flow: row wrap;    
+    justify-content: space-evenly;
+    background-color: #e3f2fd;
+`;
+
+const SingleCharDiv=styled.div<{hasFilms: boolean}>`
+    display: flex;
+    flex-direction: column;   
+    justify-content: center;
+    max-width: 30%;
+    padding: 2%;
+    margin: 1%;
+    background-color: ${(props)=>(props.hasFilms ? '#1976d2' : '#757575')};
+    color: white;
+    border: 3px #0d47a1 solid;
+    font: italic small-caps bold calc(2px + 1vw) 'Papyrus', fantasy;
+    text-align: center;
+`;
+
+export default function DisneyCharacters(props : { data:Character[] } ){
+    return (
+        <AllCharsDiv >
+            {
+                props.data.map((char: Character) =>
+                    <SingleCharDiv key={char._id} hasFilms={char.films.length > 0}>
+                        <h1>{char.name}</h1>
+                        <p>{char.films.length > 0 ? `${char.films.length} Film(s)` : "TV Only"}</p>
+                        <img src={char.imageUrl} alt={`image of ${char.name}`} />
+                    </SingleCharDiv>
+                )
+            }
+        </AllCharsDiv>
+    );
+}
